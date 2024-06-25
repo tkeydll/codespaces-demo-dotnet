b802088a8acc89ffb4092e54353a3477da577897
d275832 <kobayashi-t23@mail.dnp.co.jp>
2024-06-25 17:56:17 +0900

    chore: Refactor code to improve readability and maintainability

--- app/console/Repository.cs
+++ app/console/Repository.cs
@@ -4,10 +4,12 @@ public class Repository
 {
   public async Task RunQueryAsync(string name)
   {
+      var query = "select * from employees where employee_name = '" + name + "';";
+      Console.WriteLine(query);
+
       SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=true");
       connection.Open();
 
-      var query = "select * from employees where employee_name = '" + name + "'";
       SqlCommand command = new SqlCommand(query, connection);
       await command.ExecuteNonQueryAsync();
 
